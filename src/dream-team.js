3e13c58db233cf6511a52ea5db3c6a2a0c811f43
Zhurawlenokk <zhuravlevamary36@gmail.com>
2021-03-29 00:18:52 +0400

    add functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,36 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (members !== undefined && Array.isArray(members) && members !== null) {
+    let arr = [];
+
+    members.forEach(item => {
+      if (typeof item == 'string') {
+        item = item.toUpperCase().trim().split('');
+
+        arr.push(item[0]);
+
+      } else {
+        return false
+
+      }
+     
+    })
+
+    let str = arr.sort().join('');
+
+    return str
+
+  } else {
+    return false 
+  }
+
 };
+
+
+
+
+
+
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform (arr) {
+  let transformed = arr.forEach (function (item, i) {
+    if (item == '--discard-next') {
+      arr.splice(item[i + 1], 1);
+    }
+
+  })
+
+  console.log(transformed);
 };
+
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
