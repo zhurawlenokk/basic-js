849ba2813f6ec5196e1b9cfa68c434ffc9ce8ee8
Zhurawlenokk <zhuravlevamary36@gmail.com>
2021-03-29 00:26:11 +0400

    fix transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,14 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform (arr) {
-  let transformed = arr.forEach (function (item, i) {
-    if (item == '--discard-next') {
-      arr.splice(item[i + 1], 1);
-    }
 
-  })
-
-  console.log(transformed);
 };
 
-console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
+
